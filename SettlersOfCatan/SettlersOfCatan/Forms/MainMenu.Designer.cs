cf86df03ff5e957f6a372d09f49e2cb40a7632bf
saylor24 <siara.saylor24@gmail.com>
2016-03-28 21:28:15 -0400

    Main Menu Design

--- SettlersOfCatan/SettlersOfCatan/Forms/MainMenu.Designer.cs
+++ SettlersOfCatan/SettlersOfCatan/Forms/MainMenu.Designer.cs
@@ -28,11 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRules = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Location = new System.Drawing.Point(42, 159);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(188, 39);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // btnRules
+            // 
+            this.btnRules.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnRules.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRules.Location = new System.Drawing.Point(42, 204);
+            this.btnRules.Name = "btnRules";
+            this.btnRules.Size = new System.Drawing.Size(188, 39);
+            this.btnRules.TabIndex = 1;
+            this.btnRules.Text = "Rules";
+            this.btnRules.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Location = new System.Drawing.Point(42, 249);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(188, 39);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(500, 405);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRules);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainMenu";
             this.Text = "MainMenu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRules;
+        private System.Windows.Forms.Button btnExit;
     }
 }
