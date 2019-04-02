237fe935ab91b0921a76d10e4c2e1db2c1b12746
Anna Makuś <anm@dsr.com.pl>
2019-04-02 11:20:36 +0200

    zapis czasu wykonywania  rozgrywki

--- SettlersOfCatan/SettlersOfCatan/Program.cs
+++ SettlersOfCatan/SettlersOfCatan/Program.cs
@@ -1,6 +1,8 @@
-﻿using SettlersOfCatan.Utils;
+﻿using SettlersOfCatan.Results;
+using SettlersOfCatan.Utils;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,20 +23,23 @@ namespace SettlersOfCatan
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             AutoMapperRegister.RegisterMapping();
-            var numberOfSimulations = 4;
+            var numberOfSimulations = 2;
 
-           // while (executeGameNumber < 3)
-          //  {
+            
             Parallel.For(0, numberOfSimulations,
             new ParallelOptions { MaxDegreeOfParallelism = Convert.ToInt32(Math.Ceiling((Environment.ProcessorCount * 0.75) * 2.0)) },
             sim =>
             {
                 Console.WriteLine("{0}, Thread Id={1} START", sim, Thread.CurrentThread.ManagedThreadId);
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
                 RunBoard(new String[] { "MCTS", "Aggressive" });
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                FileWriter.SaveResultToFile(String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds,ts.Milliseconds / 10));
                 Console.WriteLine("{0}, Thread Id={1} FINISH", sim, Thread.CurrentThread.ManagedThreadId);
             });
-         //       executeGameNumber++;
-         //   }
+            Console.WriteLine("+++++++++++++++++++++++ EXE " + executeGameNumber);      
         }
 
         [STAThread]
